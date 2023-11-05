1d0705bde1ee7793b552df99b39adcd2dd9fe5a4
legoandmars <legoandmars@gmail.com>
2023-11-04 21:00:32 -0700

    bombs, explosion craters

---
+++ Assets/Runtime/Behaviour/ExplosionBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace CrossyRoad.Behaviour
+{
+    // literally only to make the crater look better
+    public class ExplosionBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _craterTransform = null!;
+        
+        public void OnEnable()
+        {
+            _craterTransform.rotation = Quaternion.Euler(0,
+                Random.Range(0f, 360f), 0);
+        }
+    }
+}
